a73a7638552de3a12046909dcd74aba40527eb27
Olga Karaivan <olga.karaivan1@gmail.com>
2019-10-17 15:54:39 +0300

    homework3

--- game1-1.py
+++ game1-1.py
@@ -1,6 +1,35 @@
 import random
+import csv
 
-class Heroes:
+class CSVFile(object):
+    
+    
+    def __init__(self, filename, delimiter=','):
+        self.filename = filename
+        self.delimiter = delimiter
+        columns = ["name", "power", "skill", "health"]
+        self.columns = columns
+        self.set_headers()
+
+
+    def set_headers(self):
+        with open(self.filename, 'w', newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=self.columns)
+            writer.writeheader()
+
+    def write_rows(self, data):
+        with open(self.filename, 'w', newline="") as file:
+            writer = csv.DictWriter(file, fieldnames=self.columns)
+            writer.writerows(data)
+
+    def read(self):
+        with open(self.filename, 'r', newline="") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                print(row["name"], " has power ", row["power"],  " skill ", row["skill"],  " health ", row["Health"])
+
+
+class Hero():
 
     def __init__(self, name, power, skill, health):
         self.name = name
@@ -8,21 +37,40 @@ class Heroes:
         self.skill = skill
         self.health = health   
 
-
-class ChoiceOfHero(Heroes):
-
-    def choose_hero():
+    def choose_hero(warriors):
         player = warriors.get(int(input('Please select warrior: 1 - strong, 2 - healthy, 3 - skill')))
-        return ('Your warrior: ', player)
+        return ('Your warrior: ', player.name)
 
     def opponent_choice():
         opponent = warriors.get(random.randint(1,3))
         return('Opponent: ', opponent)
 
+    @property
+    def health_score(self, enemy):
+        self.health = self.health - (enemy.power() * enemy.skill())
+        return self.health
 
-class Fight(Heroes, ChoiceOfHero):
+    #magic methods comparing power
+    def __gt__(self.health, enemy.health):
+        return self.health() > enemy.health()
 
-    def players_kick():
+    def __lt__(self.skill, enemy.skill):
+        return self.skill() < enemy.skill()
+
+
+    #getter method
+    def get_color(self):
+        return self._color
+
+    #setter method 
+    def set_costume_color(self, color):
+        self._color = color
+
+
+
+class Fight():
+
+    def player_kick():
         kick = int(input('Please select kick: 1 - to head, 2 - to body, 3 - to foot = '))
         return kick
 
@@ -41,16 +89,17 @@ class Fight(Heroes, ChoiceOfHero):
 
 def main():
     print('SUPER FIGHT GAME')
-    power_man = Heroes("Power man", 15, 1.0, 100)
-    healthy_man = Heroes("Healthy man", 10, 1.0, 150)
-    skill_man = Heroes("Skill man", 10, 2.0, 100)
-    warriors = {1: power_man, 2: healthy_man, 3: skill_man}
+    filename = CSVFile("warriors.csv")
+    warriors = [
+        {"name": "Power man", "power": 15, "skill": 1.0, "health": 100},
+        {"name": "Healthy man", "power": 10, "skill": 1.0, "health": 150},
+        {"name": "Skill man", "power": 10, "skill": 2.0, "health": 100}
+    ]
+    filename.write_rows(warriors)
+    filename.read()
 
-
-    player = ChoiceOfHero()
-    player.choose_hero()
-    opponent = ChoiceOfHero()
-    opponent.opponent_choice()
+    player = Hero.choose_hero(warriors)
+    opponent = Hero.opponent_choice(warriors)
     
     win = False
 
@@ -58,29 +107,26 @@ def main():
         print('Your player HP: ', player.health())
         print('Opponent    HP: ', opponent.health(), '\n')
 
-        first_action = Fight()
-        first_action.players_kick()
-        second_action = Fight()
-        second_action.player_block()
+        fight = Fight()
+        fight.player_kick()
+        fight.player_block()
         print('\n')
-        third_action = Fight()
-        third_action.opponent_kick()
-        fourth_action = Fight()
-        fourth_action.opponent_block()
+        fight.opponent_kick()
+        fight.opponent_block()
         
         
-        if first_action.players_kick() != fourth_action.opponent_block():
+        if fight.player_kick() != fight.opponent_block():
             print('You hit an opponent!')
-            opponent['health'] = opponent['health'] - (player['power'] * player['skill'])
+            opponent.health_score(player)
 
-        if second_action.player_block()!= third_action.opponent_kick():
+        if fight.player_block()!= fight.opponent_kick():
             print('Opponent hit you :( ')
-            player['health'] = player['health'] - (opponent['power'] * opponent['skill'])
+            player.health_score(opponent) 
 
-        if player['health'] < 0:
+        if player.health() < 0:
             break
 
-        if opponent['health'] < 0:
+        if opponent.health() < 0:
             win = True
             break
 
@@ -90,6 +136,7 @@ def main():
     else:
         print('GAME OVER.')
 
+main()
 
 if __name__ == '__main__':
     main()
